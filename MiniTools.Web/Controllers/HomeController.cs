a99eed4863ce57e64197589676c27e2c66d68edc
zhixian <zhixian@hotmail.com>
2021-12-18 18:51:15 +0800

    adjust HomeController

--- MiniTools.Web/Controllers/HomeController.cs
+++ MiniTools.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MiniTools.Web.Models;
 
@@ -6,50 +8,38 @@ namespace MiniTools.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private class On
+    {
+        internal static EventId NEW = new EventId(1, "New");
+        internal static EventId VIEW_HOME = new EventId(101, "View home");
+        internal static EventId VIEW_PRIVACY = new EventId(102, "View privacy");
+    }
+    private readonly ILogger<HomeController> logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
+        this.logger = logger;
+
+        logger.LogInformation(On.NEW, "{onEvent} - Controller [{controllerName}]", On.NEW, nameof(HomeController));
     }
 
     public IActionResult Index()
     {
+        logger.LogInformation(On.VIEW_HOME, "{onEvent} - Controller [{controllerName}]", On.VIEW_HOME, nameof(HomeController));
+
         return View();
     }
 
     public IActionResult Privacy()
     {
+        logger.LogInformation(On.VIEW_PRIVACY, "{onEvent} - Controller [{controllerName}]", On.VIEW_PRIVACY, nameof(HomeController));
+
         return View();
     }
 
-    //[HttpGet(Name ="Login")]
-    //[Route("/login")]
-    public IActionResult Login()
-    {
-        return View(new LoginViewModel());
-    }
-
-    [HttpPost]
-    //[Route("/login")]
-    [ValidateAntiForgeryToken]
-    public IActionResult Login(LoginViewModel model)
-    {
-        try
-        {
-            _logger.LogInformation(model.Username);
-            _logger.LogInformation(model.Password);
-            return View(model);
-        }
-        catch (Exception ex)
-        {
-            throw;
-        }
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    // public IActionResult Error()
+    // {
+    //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    // }
 }
