ba0e4690f3f8656fcab791d22cb69280b368fba3
zhixian <zhixian@hotmail.com>
2021-12-19 11:30:13 +0800

    add mongodboptions

---
+++ MiniTools.Web/Options/MongoDbOptions.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniTools.Web.Options;
+
+public class MongoDbSettings
+{  
+   public string ConnectionString { get; set; }  
+}  
+
+// public class MongoDbOptions
+// {
+//     public const string MyConfig = "MongoDbOptions";
+
+//     // [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
+//     // public string Key1 { get; set; }
+
+//     // [Range(0, 1000,ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+//     // public int Key2 { get; set; }
+//     // public int Key3 { get; set; }
+// }
