df5f6a5f1872bee4b3f2b955a08e5768dc3acf02
zhixian <zhixian@hotmail.com>
2021-12-21 00:29:12 +0800

    Update

--- MiniTools.Web/Models/AddUserViewModel.cs
+++ MiniTools.Web/Models/AddUserViewModel.cs
@@ -5,7 +5,10 @@ namespace MiniTools.Web.Models
     public class AddUserViewModel
     {
         [Display(Name = "Username", Description ="Username of user.")]
-        public string Username { get; set;}
+        public string Username { get; set; }
+
+        [Display(Name = "Password", Description = "Password of user.")]
+        public string Password { get; set; }
 
         [Display(Name = "First name", Description = "First name of user.")]
         public string FirstName { get; set; }
