bf29b64d889fc0b3ce0ba94723c580a96687ad54
zhixian <zhixian@hotmail.com>
2021-12-26 11:38:12 +0800

    add defaults

--- MiniTools.Web/Models/AddUserViewModel.cs
+++ MiniTools.Web/Models/AddUserViewModel.cs
@@ -5,20 +5,18 @@ namespace MiniTools.Web.Models
     public class AddUserViewModel
     {
         [Display(Name = "Username", Description ="Username of user.")]
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         [Display(Name = "Password", Description = "Password of user.")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [Display(Name = "First name", Description = "First name of user.")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [Display(Name = "Last name", Description = "Last name of user.")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         [Display(Name = "Email", Description = "Email address of user.")]
-        public string Email { get; set; }
-
-
+        public string Email { get; set; } = string.Empty;
     }
 }
