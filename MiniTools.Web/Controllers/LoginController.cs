899641dbd3605798269c631d8133570e92a785d9
Zhixian Ong <zhixian@hotmail.com>
2021-12-29 15:55:30 +0800

    update

--- MiniTools.Web/Controllers/LoginController.cs
+++ MiniTools.Web/Controllers/LoginController.cs
@@ -7,8 +7,6 @@ using MiniTools.Web.Models;
 using MiniTools.Web.Services;
 using MiniTools.Web.Extensions;
 using MiniTools.Web.Api.Responses;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
 
 namespace MiniTools.Web.Controllers;
 
@@ -57,80 +55,54 @@ public class LoginController : Controller
         {
             OperationResult<LoginResponse> result = await authenticationApiService.IsValidCredentialsAsync(model);
 
-            if (result.Success)
+            if (!result.Success)
             {
-
-                
-                //JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-                //if (!handler.CanReadToken(result.Payload.Jwt))
-                //{
-                //    // TODO: 
-                //}
-
-
-                IEnumerable<Claim>? claims = jwtService.GetClaims(result.Payload.Jwt);
-
-                //string jwtSecret = _configuration["JWT:SecretKey"];
-                //string jwtValidIssuer = _configuration["JWT:ValidIssuer"];
-                //string jwtValidAudience = _configuration["JWT:ValidAudience"];
-
-                //handler.ValidateToken(result.Payload.Jwt, new TokenValidationParameters
-                //{
-                //    ValidateIssuerSigningKey = true,
-                //    ValidateIssuer = true,
-                //    ValidateAudience = true,
-                //    ValidIssuer = myIssuer,
-                //    ValidAudience = myAudience,
-                //    IssuerSigningKey = mySecurityKey
-                //}, out SecurityToken validatedToken);
-
-
-                //var claims = new List<Claim>
-                //{
-                //    new Claim(ClaimTypes.Name, model.Username),
-                //    //new Claim("FullName", user.FullName),
-                //    new Claim(ClaimTypes.Role, "Administrator"),
-                //};
-
-                var claimsIdentity = new ClaimsIdentity(
-                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var authProperties = new AuthenticationProperties
-                {
-                    //AllowRefresh = <bool>,
-                    // Refreshing the authentication session should be allowed.
-
-                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    // The time at which the authentication ticket expires. A 
-                    // value set here overrides the ExpireTimeSpan option of 
-                    // CookieAuthenticationOptions set with AddCookie.
-
-                    //IsPersistent = true,
-                    // Whether the authentication session is persisted across 
-                    // multiple requests. When used with cookies, controls
-                    // whether the cookie's lifetime is absolute (matching the
-                    // lifetime of the authentication ticket) or session-based.
-
-                    //IssuedUtc = <DateTimeOffset>,
-                    // The time at which the authentication ticket was issued.
-
-                    //RedirectUri = <string>
-                    // The full path or absolute URI to be used as an http 
-                    // redirect response value.
-                };
-
-                await HttpContext.SignInAsync(
-                    CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity),
-                    authProperties);
-
-                return RedirectToAction(nameof(Index));
+                // TODO: Return invalid credentials message
+                return View(model);
             }
 
-            // this.Request.Method
-            // ControllerContext.ActionDescriptor.ControllerName
+            if ((result.Payload == null) || string.IsNullOrWhiteSpace(result.Payload.Jwt))
+            {
+                // TODO: Return invalid response
+                return View(model);
+            }
 
-            return View(model);
+            IEnumerable<Claim> claims = jwtService.GetClaims(result.Payload.Jwt);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A 
+                // value set here overrides the ExpireTimeSpan option of 
+                // CookieAuthenticationOptions set with AddCookie.
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across 
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return RedirectToAction(nameof(Index));
+
+            //return View(model);
         }
         catch (Exception ex)
         {
@@ -138,6 +110,4 @@ public class LoginController : Controller
             throw;
         }
     }
-
-
 }
