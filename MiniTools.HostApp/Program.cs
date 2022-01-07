c2664ae9b47f1f724cc2a7a46a21b2dd17e5d6c3
Zhixian Ong <zhixian@hotmail.com>
2022-01-07 16:15:34 +0800

    update

--- MiniTools.HostApp/Program.cs
+++ MiniTools.HostApp/Program.cs
@@ -1,24 +1,28 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MiniTools.HostApp.Models;
 using MiniTools.HostApp.Services;
-
-// using IHost host = Host.CreateDefaultBuilder(args)
-//     .ConfigureServices((_, services) =>
-//         services.AddTransient<ITransientOperation, DefaultOperation>()
-//             .AddScoped<IScopedOperation, DefaultOperation>()
-//             .AddSingleton<ISingletonOperation, DefaultOperation>()
-//             .AddTransient<OperationLogger>())
-//     .Build();
 
 
 var builder = Host.CreateDefaultBuilder(args);
 
 Console.WriteLine("Configuring application options...");
 
-builder.ConfigureServices(services =>
+builder.ConfigureServices((host, services) =>
 {
-    services.AddHostedService<ExampleBackgroundService>();
+    services.Configure<AzureStorageSetting>(host.Configuration.GetSection("Azure:Storage:minitools"));
+    services.Configure<AzureWebPubSubSetting>(host.Configuration.GetSection("Azure:WebPubSub:minitools"));
+
+    // services.AddHostedService<ExampleBackgroundService>();
+
+    //services.AddHostedService<QueuePublisherService>();
+    //services.AddHostedService<QueueConsumerService>();
+    
+    services.AddHostedService<PubSubConsumerService>();
+
+    services.AddHostedService<PubSubPublisherService>();
+
 });
 
 Console.WriteLine("Buiding application host...");
@@ -27,6 +31,6 @@ var host = builder.Build();
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
 
-logger.LogInformation("Hello world");
+logger.LogInformation("Running application...");
 
 host.Run();
