1438cd516b3764206f6b230d5d9f2059b29b54c4
Zhixian Ong <zhixian@hotmail.com>
2022-01-11 15:17:24 +0800

    Add DataPub And DataSub

---
+++ MiniTools.HostApp/Models/DataPublisher.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniTools.HostApp.Models;
+
+public interface IDataPublisher<T> where T : new()
+{
+    Task PublishDataAsync(CancellationToken stopToken);
+}
+
+public interface IStructDataPublisher<T> where T : struct
+{
+    Task PublishDataAsync(CancellationToken stopToken);
+}
---
+++ MiniTools.HostApp/Models/DataSubscriber.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniTools.HostApp.Models;
+
+public interface IDataSubscriber<in T> : IObserver<T> where T : new()
+{
+    void Subscribe(IObservable<T> provider);
+
+    void Unsubscribe();
+}
+
+public interface IStructDataSubscriber<in T> : IObserver<T> where T : struct
+{
+    void Subscribe(IObservable<T> provider);
+
+    void Unsubscribe();
+}
